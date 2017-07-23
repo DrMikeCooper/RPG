6c8c314fa2477fd4cfedadc36e360bac55b1de78
CoriolisBoy <coriolisboy@gmail.com>
2017-07-24 00:18:17 +0930

    Added Turner for making particles look cool

---
+++ Assets/RPG/Turner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turner : MonoBehaviour {
+
+    public float speed = 180;
+
+	// Update is called once per frame
+	void Update () {
+        transform.eulerAngles = speed * Vector3.up * Time.time;
+	}
+}
