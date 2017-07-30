f2a65755a93f611336ead04ff533ed488b797c50
CoriolisBoy <coriolisboy@gmail.com>
2017-07-30 15:28:37 +0930

    Added Debug HUD and tested different res buffs combining

---
+++ Assets/RPG/DebugHUD.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG
+{
+    public class DebugHUD : MonoBehaviour {
+
+        Text text;
+        public Character ch;
+
+        void Start()
+        {
+            text = GetComponent<Text>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+            string txt = "";
+            foreach (KeyValuePair<string, Stat> pair in ch.stats)
+            {
+                Stat s = pair.Value;
+                txt += pair.Key + ":" + s.currentValue + "\n";
+            }
+            text.text = txt;
+        }
+    }
+}
