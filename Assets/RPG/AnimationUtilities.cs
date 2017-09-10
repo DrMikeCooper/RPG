fc58103d44ae7bcfa64a5b244469c3c58d4d8a23
CoriolisBoy <coriolisboy@gmail.com>
2017-09-10 12:51:34 +0930

    Experimental stuff for editor tools

--- Assets/RPG/AnimationUtilities.cs
+++ Assets/RPG/AnimationUtilities.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Animations;
 
 namespace RPG
 {
@@ -38,27 +39,44 @@ namespace RPG
             return names;
         }
 
+        // find a named node in an animator
+        public AnimatorState FindState(Animator animator, string name)
+        {
+            AnimatorController ac = animator.runtimeAnimatorController as AnimatorController;
+            for (int i = 0; i < animator.layerCount; i++)
+            {
+                ChildAnimatorState[] aStates = ac.layers[i].stateMachine.states;
+                for (int j = 0; j < aStates.Length; j++)
+                {
+                    if (aStates[j].state.name == name)
+                        return aStates[j].state;
+                }
+            }
+
+            return null;
+        }
+
         public static void ProcessAnimations(Animator animator, string[] nodeNames, System.Action<AnimationClip> fn)
         {
             // iterate over the animator graph's node, checking if the node is one we'll be triggering as a power animation
             // and stick a 
-            UnityEditor.Animations.AnimatorController ac = animator.runtimeAnimatorController as UnityEditor.Animations.AnimatorController;
+            AnimatorController ac = animator.runtimeAnimatorController as AnimatorController;
             for (int i = 0; i < animator.layerCount; i++)
             {
-                UnityEditor.Animations.ChildAnimatorState[] aStates = ac.layers[i].stateMachine.states;
+                ChildAnimatorState[] aStates = ac.layers[i].stateMachine.states;
                 for (int j = 0; j < aStates.Length; j++)
                 {
-                    UnityEditor.Animations.ChildAnimatorState aState = aStates[j];
+                    ChildAnimatorState aState = aStates[j];
 
                     // check if its a power anim, by comparing the node name with known power anim names
-                    bool isPower = false;
+                    bool onList = false;
                     for (int k = 0; k < nodeNames.Length; k++)
                     {
                         if (aState.state.name == nodeNames[k])
-                            isPower = true;
+                            onList = true;
                     }
 
-                    if (isPower)
+                    if (onList)
                     {
                         // see if the node has an animation
                         AnimationClip clip = aState.state.motion as AnimationClip;
@@ -66,6 +84,48 @@ namespace RPG
                         if (clip)
                         {
                             fn.Invoke(clip);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void AddTransitionToState(Animator animator, string[] nodeNames, AnimatorStateTransition transition)
+        {
+            // iterate over the animator graph's node, checking if the node is one we'll be triggering as a power animation
+            // and stick a 
+            UnityEditor.Animations.AnimatorController ac = animator.runtimeAnimatorController as UnityEditor.Animations.AnimatorController;
+            for (int i = 0; i < animator.layerCount; i++)
+            {
+                ChildAnimatorState[] aStates = ac.layers[i].stateMachine.states;
+                for (int j = 0; j < aStates.Length; j++)
+                {
+                    UnityEditor.Animations.ChildAnimatorState aState = aStates[j];
+                   
+                    // check if its a power anim, by comparing the node name with known power anim names
+                    bool onList = false;
+                    for (int k = 0; k < nodeNames.Length; k++)
+                    {
+                        if (aState.state.name == nodeNames[k])
+                            onList = true;
+                    }
+
+                    if (onList)
+                    {
+                        // check if we already go to the state in the required transition
+                        bool alreadySet = false;
+                        for (int k = 0; k < aState.state.transitions.Length; k++)
+                        {
+                            if (aState.state.transitions[k].destinationState == transition.destinationState)
+                                alreadySet = true;
+                        }
+
+                        // if not, add the required transition
+                        if (!alreadySet)
+                            aState.state.AddTransition(transition);
+                        else
+                        {
+                            // make sure we've got all the conditions?
                         }
                     }
                 }
