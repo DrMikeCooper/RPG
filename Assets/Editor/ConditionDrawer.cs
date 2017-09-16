b89bed6f53c0a0eba34d57448c5632658863a65b
CoriolisBoy <coriolisboy@gmail.com>
2017-09-17 00:51:08 +0930

    Nice Inspector stuff for AIConditions

---
+++ Assets/Editor/ConditionDrawer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using RPG;
+
+[CustomPropertyDrawer(typeof(AINode.AICondition))]
+public class ConditionDrawer : PropertyDrawer
+{
+    // Draw the property inside the given rect
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        // Using BeginProperty / EndProperty on the parent property means that
+        // prefab override logic works on the entire property.
+        EditorGUI.BeginProperty(position, label, property);
+
+        // Draw label
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+        // Don't make child fields be indented
+        var indent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+
+        // Calculate rects
+        var typeRect = new Rect(position.x, position.y, 100, position.height);
+        var labelRect = new Rect(position.x + 105, position.y, 30, position.height);
+        var thresholdRect = new Rect(position.x + 145, position.y, 60, position.height);
+        var reverseRect = new Rect(position.x + 205, position.y, 50, position.height);
+
+        SerializedProperty typeProp = property.FindPropertyRelative("type");
+        SerializedProperty reverseProp = property.FindPropertyRelative("reverse");
+        // Draw fields - passs GUIContent.none to each so they are drawn without labels
+        EditorGUI.PropertyField(typeRect, typeProp, GUIContent.none);
+        if (typeProp.intValue > 0)
+        {
+            EditorGUI.LabelField(labelRect, reverseProp.boolValue ? " <" : ">");
+            EditorGUI.PropertyField(thresholdRect, property.FindPropertyRelative("threshold"), GUIContent.none);
+            EditorGUI.PropertyField(reverseRect, reverseProp, GUIContent.none);
+        }
+
+        // Set indent back to what it was
+        EditorGUI.indentLevel = indent;
+
+        EditorGUI.EndProperty();
+    }
+}
+
