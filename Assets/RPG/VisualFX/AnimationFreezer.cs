4ff1bc6a076540aa68f16225202709ac88a7b9a4
CoriolisBoy <coriolisboy@gmail.com>
2017-10-04 22:41:51 +1030

    Animation freeze component

---
+++ Assets/RPG/VisualFX/AnimationFreezer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG
+{
+    public class AnimationFreezer : MonoBehaviour
+    {
+        Animator animator;
+
+        // Use this for initialization
+        void Start()
+        {
+            animator = transform.parent.GetComponent<Animator>();
+            TurnOn();
+        }
+
+        // Use this for initialization
+        void OnEnable()
+        {
+            TurnOn();
+        }
+
+        void OnDisable()
+        {
+            TurnOff();
+        }
+
+        void TurnOn()
+        {
+            if (animator)
+                animator.enabled = false;
+        }
+
+        void TurnOff()
+        {
+            animator.enabled = true;
+        }
+    }
+}
